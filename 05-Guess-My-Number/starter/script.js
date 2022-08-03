8db966404c4e4cd89cc735ca7f9c9263db83d8af
tabby-lab <tabby.garcia1@gmail.com>
2022-08-03 14:59:37 -0500

    push test

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random()*20) +1;
+document.querySelector('.number').textContent = secretNumber;
+
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No number!';
+    }else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'yay correct number!';
+
+    }else if(guess > secretNumber) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Too high';
+        score = score - 1;
+        document.querySelector('.score').textContent = score;
+        } else{
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
+    }else if(guess < secretNumber){
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Too low';
+        score = score - 1;
+        document.querySelector('.score').textContent = score;
+        } else{
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
+       
+    }
+
+});
+//test
