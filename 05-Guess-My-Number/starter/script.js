7f87d6bc81dd9682ff1794519faa361fedbf9fbb
tabby-lab <tabby.garcia1@gmail.com>
2022-08-05 15:06:05 -0500

    reset game functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random()*20) +1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random()*20) +1;
+
+//shows the secret num
+// document.querySelector('.number').textContent = secretNumber;
+
 
 
 let score = 20;
@@ -19,6 +22,8 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.message').textContent = 'yay correct number!';
 
         document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').textContent = secretNumber;
 
         //when guess is too high
     }else if(guess > secretNumber) {
@@ -45,4 +50,22 @@ document.querySelector('.check').addEventListener('click', function(){
     }
 
 });
+
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20) +1;
+    document.querySelector('.message').textContent = 'Start guessing';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+
+
+ })
+
+
+
+
+
 //test
