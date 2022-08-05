5f18a2a002d6b1b610fcd23d289302c0847c849d
tabby-lab <tabby.garcia1@gmail.com>
2022-08-05 14:15:31 -0500

    changing background color when a user wins

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,11 +10,17 @@ document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
+    //when there is no input
     if(!guess){
         document.querySelector('.message').textContent = 'No number!';
+
+        //when player wins
     }else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'yay correct number!';
 
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        //when guess is too high
     }else if(guess > secretNumber) {
         if(score > 1) {
             document.querySelector('.message').textContent = 'Too high';
@@ -24,6 +30,8 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.message').textContent = 'You lost the game';
             document.querySelector('.score').textContent = 0;
         }
+
+        //when guess is too low
     }else if(guess < secretNumber){
         if(score > 1) {
             document.querySelector('.message').textContent = 'Too low';
